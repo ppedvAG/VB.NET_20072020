eee960004ca65c78a51f1c2edee4bff2548d4118
André <AndreR@ppedv.de>
2020-07-20 16:32:38 +0200

    Dvd Screensaver

---
+++ DVDScreensaver/DVDScreensaver/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LimeGreen
+        Me.Button1.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.DarkViolet
+        Me.Button1.Location = New System.Drawing.Point(134, 78)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(428, 88)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Klick mich"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Aqua
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ DVDScreensaver/DVDScreensaver/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Du hast mich geklickt mit dem Speed:" & speedX)
+        MachButtonBunt()
+    End Sub
+
+    Private Sub Button1_MouseEnter(sender As Object, e As EventArgs) Handles Button1.MouseEnter
+        Button1.BackColor = Color.OrangeRed
+        'Button1.Left = Button1.Left + 110
+        'Button1.Left += 110
+    End Sub
+
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+        Button1.BackColor = Color.Chartreuse
+    End Sub
+
+    Sub MachButtonBunt()
+        Dim ran As New Random()
+
+        Button1.BackColor = Color.FromArgb(ran.Next(255), ran.Next(255), ran.Next(255))
+        BackColor = Color.FromArgb(ran.Next(255), ran.Next(255), ran.Next(255))
+
+        Task.Run(Sub() Console.Beep(220, 200))
+
+
+    End Sub
+
+    Dim speedX As Integer = 4
+    Dim speedY As Integer = 4
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Text = Button1.Left & "x" & Button1.Top
+        Button1.Left += speedX
+
+        If Button1.Left + Button1.Width >= ClientSize.Width Or Button1.Left <= 0 Then
+            speedX *= -1
+            MachButtonBunt()
+        End If
+
+        Button1.Top += speedY
+
+        If Button1.Top + Button1.Height >= ClientSize.Height Or Button1.Top <= 0 Then
+            speedY *= -1
+            MachButtonBunt()
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'MessageBox.Show("Viel Spass am " & Date.Now.DayOfWeek.ToString())
+
+        'Dim tag As Date = Date.Now.AddDays(3)
+        Dim tag As Date = New Date(2020, 7, 26)
+
+        Select Case tag.DayOfWeek
+            Case DayOfWeek.Monday
+                Button1.Text = "🙇‍"
+            Case DayOfWeek.Wednesday
+                Button1.Text = "🍔"
+            Case DayOfWeek.Friday
+                Button1.Text = "🧦🧦"
+            Case DayOfWeek.Saturday
+                Button1.Text = "🍻🍻"
+            Case DayOfWeek.Sunday
+                Button1.Text = "🍆"
+            Case Else
+                Button1.Text = "🍗"
+        End Select
+
+    End Sub
+End Class
