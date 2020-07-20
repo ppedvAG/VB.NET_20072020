7ff5f5d3e9db44f4a5833e0b6de8b7bf572fa41e
André <AndreR@ppedv.de>
2020-07-20 13:42:36 +0200

    Parsing

---
+++ StringParsing/StringParsing/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("*** String Parser 5000 v1.0 ***")
+
+        Console.WriteLine("Bitte ein Zahl eingeben:")
+        Dim eingabe As String = Console.ReadLine()
+        Console.WriteLine("Eingabe: " & eingabe)
+
+        Dim eingabeAlsInt As Integer = Integer.Parse(eingabe)
+        Console.WriteLine("Eingabe als Int:" & eingabeAlsInt)
+        Console.WriteLine("Das doppelte ist:" & eingabeAlsInt * 2)
+
+
+
+        Console.WriteLine("Ende")
+        Console.ReadKey()
+    End Sub
+
+End Module
