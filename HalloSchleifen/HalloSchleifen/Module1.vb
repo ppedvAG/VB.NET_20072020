3e91a81da4d7e8a018894710b6ed1f63ee00427f
André <AndreR@ppedv.de>
2020-07-21 11:37:14 +0200

    Schleifen: for und while

---
+++ HalloSchleifen/HalloSchleifen/Module1.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("*** Hallo Schleifen ***")
+
+
+        'zählergesteuerte Schleife
+        For index = 1 To 100 Step 4
+            Console.WriteLine(index)
+        Next
+
+        Dim text = "Die ist ein toller Text"
+
+        Dim buchstabe As Char = text(4)
+
+        Dim sw = New IO.StreamWriter("meineDaten.txt")
+
+        For index = 0 To text.Length - 1
+            Console.WriteLine(text(index))
+            sw.WriteLine("Char an Index " & index & ": " & text(index))
+        Next
+
+        sw.Close()
+        Console.WriteLine("=================================================0")
+
+        'while schleife
+        Dim sr = New IO.StreamReader("meineDaten.txt")
+        While Not sr.EndOfStream
+            Console.WriteLine("Datei: " & sr.ReadLine())
+        End While
+
+        Dim z = 0
+        While True
+            If z Mod 1000000 = 0 Then
+                Console.WriteLine(z & " " & Date.Now)
+            End If
+            z += 1
+        End While
+
+        Console.WriteLine("Ende")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
