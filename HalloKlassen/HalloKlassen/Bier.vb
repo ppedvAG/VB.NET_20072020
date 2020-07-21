15be72b8d2684a3b4674f1dfbd1f80429726c166
André <AndreR@ppedv.de>
2020-07-21 11:37:24 +0200

    Bier ist klasse

---
+++ HalloKlassen/HalloKlassen/Bier.vb
@@ -0,0 +1,7 @@
+﻿Public Class Bier
+    Public Brauerei As String
+    Public Sorte As String
+    Public Art As String
+    Public Alk As Double
+    Public Geburt As Date
+End Class
---
+++ HalloKlassen/HalloKlassen/Module1.vb
@@ -0,0 +1,65 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("*** Bier! 🍻 ***")
+
+        Dim hoepfner As Bier 'deklaration
+        hoepfner = New Bier() 'instanzierung
+
+        hoepfner.Brauerei = "Hoepfner"
+        hoepfner.Sorte = "Pilsner"
+        hoepfner.Art = "Pils"
+        hoepfner.Alk = 4.9
+        hoepfner.Geburt = New Date(1798, 1, 1)
+
+        Dim welde As New Bier() 'deklaration + instanzierung
+        welde.Brauerei = "Welde"
+        welde.Sorte = "No. 1"
+        welde.Art = "Pils"
+        welde.Alk = 4.9
+        welde.Geburt = New Date(1752, 1, 1)
+
+
+        ZeigeBier(hoepfner)
+        ZeigeBier(welde)
+        Verwässern(welde, 2)
+        Verwässern(welde)
+        ZeigeBier(welde)
+        ZeigeBier(CreateRothaus())
+
+        Console.WriteLine("Ende")
+        Console.ReadKey()
+    End Sub
+
+    Function CreateRothaus() As Bier
+        Dim b = New Bier
+        b.Brauerei = "Badische Staatbrauerei Rothaus AG"
+        b.Sorte = "Tannenzäpfle"
+        b.Art = "Pils"
+        b.Alk = 5.1
+        b.Geburt = New Date(1791, 1, 1)
+        Return b
+    End Function
+
+    Sub ZeigeBier(einBier As Bier)
+        Console.WriteLine("------ ZeigeBier ------")
+        Console.WriteLine(einBier.Brauerei & " " & einBier.Sorte & "(" & einBier.Art & ")")
+        Console.WriteLine(einBier.Alk.ToString("f2") & "% " & einBier.Geburt.Year)
+        Console.WriteLine("-----------------------")
+    End Sub
+
+    Sub Verwässern(einBier As Bier, faktor As Integer)
+        einBier.Alk /= faktor
+    End Sub
+
+    Sub Verwässern(einBier As Bier)
+        einBier.Alk /= 2
+    End Sub
+
+    Function GibtMirFünf() As Integer
+        Return 5
+    End Function
+
+
+
+End Module
